4f5ed8ee3d25aecfbda67a4427e4dc6d86245a7e
khilnani <nik@khilnani.org>
2016-05-01 23:10:11 -0400

    moved from https://github.com/khilnani/pythonista-scripts

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# hipchat.py
-Script to improve hipchat access on iOS — Edit
+## hipchat.py
+
+A python script to display unread hipchat rooms/conversations.
+
+While designed for the iOS [Pythonista](http://omz-software.com/pythonista/) application, the script runs on any linux/mac os environment.
+
+### Configuration
+
+- Rename `hipchat.sample.conf` to `hipchat.conf` and update values
+
+### Usage
+
+- When you run the script for the first time, it will request a Personal Access token
+to access Hipchat on your behalf
+- *This scripts only requires read access.*
+- Hipchat allows you set configure access permissions eg. read, write, admin.
+- Get a personal access token from: https://www.hipchat.com/account/api
+
+iOS / Pythonista
+- Simple run this script in Pythonista.
+
+Linux/Mac OS
+- Run this script in a linux/os x terminal.
+
+## Installation
+
+- Download or clone the github repo, or:
+  - Pythonista console: `import urllib2; exec urllib2.urlopen('http://khl.io/hipchat-py').read()`
+  - Linux/Mac OS Terminal: `python -c "import urllib2; exec urllib2.urlopen('http://khl.io/hipchat-py').read()"`
---
+++ install.py
@@ -0,0 +1,77 @@
+import os
+import platform
+import sys
+import logging
+import urllib2
+
+############################################
+
+INSTALL_DIR = os.getcwd()
+
+REPO_NAME = 'hipchat.py'
+SCRIPT_NAME = 'hipchat.py'
+GITHUB_MASTER = 'https://raw.githubusercontent.com/khilnani/%s/master/' % REPO_NAME
+
+
+############################################
+
+machine = platform.machine()
+print 'Platform system:' + machine
+
+print('Installing to: %s' % INSTALL_DIR)
+
+if 'iP' in machine:
+    BASE_DIR = os.path.expanduser('~/Documents')
+else:
+    BASE_DIR = os.getcwd()
+
+############################################
+
+try:
+    import console
+except ImportError:
+    class console (object):
+        @staticmethod
+        def hud_alert(message, icon=None, duration=None):
+            print message
+
+        @staticmethod
+        def input_alert(title, message=None, input=None, ok_button_title=None, hide_cancel_button=False):
+            message = '' if message == None else message
+            ret = input
+            try:
+                ret = raw_input(title + '' + message + ' :')
+            except Exception as e:
+                print e
+            return ret
+
+############################################
+
+def setup_logging(log_level='INFO'):
+    log_format = "%(message)s"
+    logging.basicConfig(format=log_format, level=log_level)
+
+def download_file(src, dest):
+    logging.info('Downloading %s' % (src))
+    file_content = urllib2.urlopen(src).read()
+    logging.info('Writing %s' % dest)
+    f = open(dest, 'w')
+    f.write(file_content)
+    f.close()
+
+############################################
+
+def download():
+    download_file(GITHUB_MASTER+SCRIPT_NAME, os.path.join(INSTALL_DIR, SCRIPT_NAME))
+    logging.info('Done.')
+
+############################################
+
+def main():
+    setup_logging()
+    download()
+
+############################################
+
+if __name__ == '__main__':
+    main()
